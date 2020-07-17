edf6c9fab4d82c11c1a1f51a50c367ec6c1cff4c
1000ch <shogosensui@gmail.com>
2020-07-17 21:50:00 +0900

    Tweaks

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const test = require('ava');
 const execa = require('execa');
+const pkg = require('./package');
 
 const expected = [
   'readme.md',
@@ -8,6 +10,19 @@ const expected = [
   '## Usage',
   '## License'
 ].join('\n');
+
+test('outputs version', async t => {
+  const {stdout} = await execa.node('./cli.js', ['-v']);
+
+  t.is(stdout, pkg.version);
+});
+
+test('outputs usage', async t => {
+  const {stdout} = await execa.node('./cli.js', ['-h']);
+  const help = fs.readFileSync('./usage.txt', 'utf-8');
+
+  t.is(stdout, help);
+});
 
 test('accepts markdown file', async t => {
   const {stdout} = await execa.node('./cli.js', ['./readme.md']);
