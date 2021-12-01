d80cd1e6a9a5d0f085b1f53dbcf470e09b7bc25b
1000ch <shogosensui@gmail.com>
2021-12-01 14:07:34 +0900

    chore: meta tweaks

--- test.js
+++ test.js
@@ -1,31 +1,31 @@
 import {promises as fs} from 'node:fs';
 import test from 'ava';
-import execa from 'execa';
+import {execa} from 'execa';
 
 const expected = [
   'readme.md',
-  '# markdown-headings-cli',
+  '# markdown-headings-cli GitHub Actions Status',
   '## Install',
   '## Usage',
   '## License',
 ].join('\n');
 
 test('outputs version', async t => {
-  const {stdout} = await execa.node('./cli.js', ['-v']);
+  const {stdout} = await execa('./cli.js', ['-v']);
   const {version} = JSON.parse(await fs.readFile('package.json', 'utf8'));
 
   t.is(stdout, version);
 });
 
 test('outputs usage', async t => {
-  const {stdout} = await execa.node('./cli.js', ['-h']);
+  const {stdout} = await execa('./cli.js', ['-h']);
   const help = await fs.readFile('usage.txt', 'utf-8');
 
   t.is(stdout, help);
 });
 
 test('accepts markdown file', async t => {
-  const {stdout} = await execa.node('./cli.js', ['./readme.md']);
+  const {stdout} = await execa('./cli.js', ['./readme.md']);
 
   t.is(stdout, expected);
 });
